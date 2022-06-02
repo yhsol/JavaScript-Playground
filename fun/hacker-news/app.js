4f20585c6bb6c8c451d802450f2a63e936d0ec13
paulyangg <paul@protopie.io>
2022-06-02 17:21:38 +0900

    feat: add lilnk and refactoring

--- fun/hacker-news/app.js
+++ fun/hacker-news/app.js
@@ -1,15 +1,58 @@
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
-const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+const content = document.createElement('div');
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-const newsFeed = JSON.parse(ajax.response);
-const ul = document.createElement('ul');
-
-for (let i = 0; i < 10; i++) {
-    const li = document.createElement('li');
-    li.innerHTML = newsFeed[i].title;
-    ul.appendChild(li);
+function getData(url) {
+    ajax.open('GET', url, false);
+    ajax.send();
+    
+    return JSON.parse(ajax.response);
 }
 
-document.getElementById('root').appendChild(ul);
+// ajax.open('GET', NEWS_URL, false);
+// ajax.send();
+
+// const newsFeed = JSON.parse(ajax.response);
+const newsFeed = getData(NEWS_URL);
+const ul = document.createElement('ul');
+
+window.addEventListener('hashchange', () => {
+    const id = location.hash.substr(1);
+    // ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+    // ajax.send();
+
+    // const newsContent = JSON.parse(ajax.response);
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
+    const title = document.createElement('h1');
+
+    title.innerText = newsContent.title;
+
+    content.appendChild(title);
+})
+
+for (let i = 0; i < 10; i++) {
+    const div = document.createElement('div');
+    // const li = document.createElement('li');
+    // const a = document.createElement('a');
+
+    div.innerHTML = 
+    `
+    <li>
+        <a href="#${newsFeed[i].id}">
+            ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+    </li>
+    `;
+
+    // a.href = `#${newsFeed[i].id}`;
+    // a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+    // li.appendChild(a);
+    // ul.appendChild(li);
+    ul.appendChild(div.firstElementChild);
+}
+
+container.appendChild(ul);
+container.appendChild(content);
