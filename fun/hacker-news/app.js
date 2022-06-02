aa3cc615acfdbfbd3add3ac4229177d8401b7af8
paulyangg <paul@protopie.io>
2022-06-02 17:22:40 +0900

    refactor

--- fun/hacker-news/app.js
+++ fun/hacker-news/app.js
@@ -11,22 +11,13 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
-// ajax.open('GET', NEWS_URL, false);
-// ajax.send();
-
-// const newsFeed = JSON.parse(ajax.response);
 const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', () => {
-    const id = location.hash.substr(1);
-    // ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-    // ajax.send();
+    const newsContent = getData(CONTENT_URL.replace('@id', location.hash.substr(1)));
 
-    // const newsContent = JSON.parse(ajax.response);
-    const newsContent = getData(CONTENT_URL.replace('@id', id));
     const title = document.createElement('h1');
-
     title.innerText = newsContent.title;
 
     content.appendChild(title);
@@ -34,8 +25,6 @@ window.addEventListener('hashchange', () => {
 
 for (let i = 0; i < 10; i++) {
     const div = document.createElement('div');
-    // const li = document.createElement('li');
-    // const a = document.createElement('a');
 
     div.innerHTML = 
     `
@@ -46,11 +35,6 @@ for (let i = 0; i < 10; i++) {
     </li>
     `;
 
-    // a.href = `#${newsFeed[i].id}`;
-    // a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-
-    // li.appendChild(a);
-    // ul.appendChild(li);
     ul.appendChild(div.firstElementChild);
 }
 
