a2e2549e91f5cf1a6781b330854b1dc16a7638e5
paulyangg <paul@protopie.io>
2022-06-03 16:20:04 +0900

    feat: route and page

--- fun/hacker-news/app.js
+++ fun/hacker-news/app.js
@@ -4,6 +4,10 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
+const store = {
+    currentPage: 1,
+}
+
 function getData(url) {
     ajax.open('GET', url, false);
     ajax.send();
@@ -11,32 +15,69 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+function newsFeed() {
+    const newsFeed = getData(NEWS_URL);
+    const newsList = [];
 
-window.addEventListener('hashchange', () => {
-    const newsContent = getData(CONTENT_URL.replace('@id', location.hash.substr(1)));
-
-    const title = document.createElement('h1');
-    title.innerText = newsContent.title;
-
-    content.appendChild(title);
-})
-
-for (let i = 0; i < 10; i++) {
-    const div = document.createElement('div');
-
-    div.innerHTML = 
-    `
-    <li>
-        <a href="#${newsFeed[i].id}">
-            ${newsFeed[i].title} (${newsFeed[i].comments_count})
-        </a>
-    </li>
+    let template = `
+        <div>
+            <h1>Hacker News</h1>
+            <ul>
+                {{__news_feed__}}
+            </ul>
+            <div>
+                <a href="#/page/{{__prev_page__}}">prev</a>
+                <a href="#/page/{{__next_page__}}">next</a>
+            </div>
+        </div>
     `;
 
-    ul.appendChild(div.firstElementChild);
+    newsList.push('<ul>')
+    for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+        newsList.push(`
+        <li>
+            <a href="#/show/${newsFeed[i].id}">
+                ${newsFeed[i].title} (${newsFeed[i].comments_count})
+            </a>
+        </li>
+        `);
+    }
+    newsList.push('</ul>')
+    newsList.push(`
+    <div>
+        <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">prev</a>
+        <a href="#/page/${store.currentPage + 1}">next</a>
+    </div>
+    `);
+    container.innerHTML = newsList.join('');
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+    const newsContent = getData(CONTENT_URL.replace('@id', location.hash.substr(7)));
+    
+    container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+
+    <div>
+        <a href="#/page/${store.currentPage}">to list</a>
+    </div>    
+    `;
+}
+
+function router() {
+    const routerPath = location.hash;
+    
+    if (routerPath === '') {
+        newsFeed();
+    } else if (routerPath.indexOf('#/page') >= 0) {
+        store.currentPage = Number(routerPath.substr(7));
+        newsFeed();
+    } else {
+        newsDetail();
+    }
+
+}
+
+window.addEventListener('hashchange', router)
+
+router();
