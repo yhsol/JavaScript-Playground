2278bbdd30137074d5b77c37fb5e0674c361a10f
paulyangg <paul@protopie.io>
2022-06-03 17:18:20 +0900

    feat: add style and comments section

--- fun/hacker-news/app.js
+++ fun/hacker-news/app.js
@@ -19,49 +19,117 @@ function newsFeed() {
     const newsFeed = getData(NEWS_URL);
     const newsList = [];
 
+    // 변경할 부분을 마킹해 두고 template.replace 를 통해 변경한다.
     let template = `
-        <div>
-            <h1>Hacker News</h1>
-            <ul>
+        <div class="bg-gray-600 min-h-screen">
+            <div class="bg-white text-xl">
+                <div class="mx-auto px-4">
+                    <div class="flex justify-between items-center py-6">
+                        <div class="flex justify-start">
+                            <h1 class="font-extrabold">Hacker News</h1>
+                        </div>
+                        <div class="items-center justify-end">
+                            <a href="#/page/{{__prev_page__}}" class="text-gray-500">
+                                Previous
+                            </a>
+                            <a href="#/page/{{__next_page__}}" class="text-gray-500">
+                                Next
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="p-4 text-2xl text-gray-700">
                 {{__news_feed__}}
-            </ul>
-            <div>
-                <a href="#/page/{{__prev_page__}}">prev</a>
-                <a href="#/page/{{__next_page__}}">next</a>
             </div>
         </div>
     `;
 
-    newsList.push('<ul>')
     for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
         newsList.push(`
-        <li>
-            <a href="#/show/${newsFeed[i].id}">
-                ${newsFeed[i].title} (${newsFeed[i].comments_count})
-            </a>
-        </li>
+        <div class="p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+            <div class="flex">
+                <div class="flex-auto">
+                    <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>
+                </div>
+                <div class="text-center text-sm">
+                    <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${newsFeed[i].comments_count}</div>
+                </div>
+            </div>
+            <div class="flex mt-3">
+                <div class="grid grid-cols-3 text-sm text-gray-500">
+                    <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
+                    <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
+                    <div><i class="fas fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
+                </div>
+            </div>
+        </div>
         `);
     }
-    newsList.push('</ul>')
-    newsList.push(`
-    <div>
-        <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">prev</a>
-        <a href="#/page/${store.currentPage + 1}">next</a>
-    </div>
-    `);
-    container.innerHTML = newsList.join('');
+
+    template = template.replace('{{__news_feed__}}', newsList.join(''));
+    template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
+    template = template.replace('{{__next_page__}}', store.currentPage +1);
+
+    container.innerHTML = template;
 }
 
 function newsDetail() {
     const newsContent = getData(CONTENT_URL.replace('@id', location.hash.substr(7)));
     
-    container.innerHTML = `
-    <h1>${newsContent.title}</h1>
+    let template = `
+        <div class="bg-gray-600 min-h-screen pb-8">
+            <div class="bg-white text-xl">
+                <div class="mx-auto px-4">
+                    <div class="flex justify-between items-center py-6">
+                        <div class="flex justify-start">
+                            <h1 class="font-extrabold">Hacker News</h1>
+                        </div>
+                        <div class="items-center justify-end">
+                            <a href="#/page/${store.currentPage}" class="text-gray-500">
+                                <i class="fa fa-times"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-    <div>
-        <a href="#/page/${store.currentPage}">to list</a>
-    </div>    
+            <div class="h-full border rounded-xl bg-white m-6 p-4">
+                <h2>${newsContent.title}</h2>
+                <div class="text-gray-400 h-20">
+                    ${newsContent.content}
+                </div>
+
+                {{__comments__}}
+
+            </div>
+        </div>
     `;
+
+    function makeComment(comments, called = 0) {
+        const commentString = [];
+        for (let i = 0; i < comments.length; i++) {
+            commentString.push(`
+                <div style="padding-left: ${40 * called}px;" class="mt-4">
+                    <div class="text-gray-400">
+                        <i class="fa fa-sort-up mr-2"></i>
+                        <strong>${comments[i].user}</strong> ${comments[i].time_ago}
+                    </div>
+                    <p class="text-gray-700">${comments[i].content}</p>
+                </div>
+            `);
+
+            // comments 를 다시 호출하여 대댓글을 출력한다.
+            if (comments[i].comments.length > 0) {
+                commentString.push(makeComment(comments[i].comments, called + 1));
+            }
+        }
+
+        return commentString.join('');
+    }
+
+    template = template.replace('{{__comments__}}', makeComment(newsContent.comments));
+    container.innerHTML = template;
 }
 
 function router() {
