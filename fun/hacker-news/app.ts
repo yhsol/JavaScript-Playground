6f5ed076c27b49c900540dc14a60928315ea6c14
paulyangg <paul@protopie.io>
2022-06-10 16:58:11 +0900

    feat: js to ts

--- fun/hacker-news/app.ts
+++ fun/hacker-news/app.ts
@@ -1,22 +1,51 @@
-const container = document.getElementById('root');
-const ajax = new XMLHttpRequest();
-const content = document.createElement('div');
+type Store = {
+    currentPage: number,
+    feeds: NewsFeed[],
+}
+
+type News = {
+    id: number,
+    title: string,
+    time_ago: string,
+    url: string;
+    user: string;
+    content: string;
+}
+
+type NewsFeed = News & {
+    points: number,
+    comments_count: number,
+    read?: boolean,
+}
+
+type NewsDetail = News & {
+    comments: NewsComment[];
+}
+
+type NewsComment = News & {
+    level: number,
+    comments: NewsComment[],
+}
+
+const container: HTMLElement | null = document.getElementById('root');
+const ajax: XMLHttpRequest = new XMLHttpRequest();
+const content: HTMLDivElement = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-const store = {
+const store: Store = {
     currentPage: 1,
     feeds: [],
 }
 
-function getData(url) {
+function getData<AjaxResponse>(url: string): AjaxResponse {
     ajax.open('GET', url, false);
     ajax.send();
     
     return JSON.parse(ajax.response);
 }
 
-function makeFeeds(feeds) {
+function makeFeeds(feeds: NewsFeed[]) {
     for (let i = 0; i < feeds.length; i++) {
         feeds[i].read = false;
     }
@@ -24,12 +53,20 @@ function makeFeeds(feeds) {
     return feeds;
 }
 
+function updateView(html: string) {
+    if (container) {
+        container.innerHTML = html;
+    } else {
+        console.log('container is null');
+    }
+}
+
 function newsFeed() {
-    let newsFeed = store.feeds; // getData(NEWS_URL);
+    let newsFeeds: NewsFeed[] = store.feeds; // getData(NEWS_URL);
     const newsList = [];
 
-    if (newsFeed.length === 0) {
-        newsFeed = store.feeds = makeFeeds(getData(NEWS_URL));
+    if (newsFeeds.length === 0) {
+        newsFeeds = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));
     }
 
     // 변경할 부분을 마킹해 두고 template.replace 를 통해 변경한다.
@@ -60,20 +97,20 @@ function newsFeed() {
 
     for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
         newsList.push(`
-        <div class="p-6 ${newsFeed[i].read ? 'bg-gray-200' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+        <div class="p-6 ${newsFeeds[i].read ? 'bg-gray-200' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
             <div class="flex">
                 <div class="flex-auto">
-                    <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>
+                    <a href="#/show/${newsFeeds[i].id}">${newsFeeds[i].title}</a>
                 </div>
                 <div class="text-center text-sm">
-                    <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${newsFeed[i].comments_count}</div>
+                    <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${newsFeeds[i].comments_count}</div>
                 </div>
             </div>
             <div class="flex mt-3">
                 <div class="grid grid-cols-3 text-sm text-gray-500">
-                    <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
-                    <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
-                    <div><i class="fas fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
+                    <div><i class="fas fa-user mr-1"></i>${newsFeeds[i].user}</div>
+                    <div><i class="fas fa-heart mr-1"></i>${newsFeeds[i].points}</div>
+                    <div><i class="fas fa-clock mr-1"></i>${newsFeeds[i].time_ago}</div>
                 </div>
             </div>
         </div>
@@ -81,15 +118,15 @@ function newsFeed() {
     }
 
     template = template.replace('{{__news_feed__}}', newsList.join(''));
-    template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
-    template = template.replace('{{__next_page__}}', store.currentPage +1);
+    template = template.replace('{{__prev_page__}}', String(store.currentPage > 1 ? store.currentPage - 1 : 1));
+    template = template.replace('{{__next_page__}}', String(store.currentPage +1));
 
-    container.innerHTML = template;
+    updateView(template);
 }
 
 function newsDetail() {
     const id = location.hash.substr(7)
-    const newsContent = getData(CONTENT_URL.replace('@id', id));
+    const newsContent = getData<NewsDetail>(CONTENT_URL.replace('@id', id));
 
     let template = `
         <div class="bg-gray-600 min-h-screen pb-8">
@@ -127,30 +164,30 @@ function newsDetail() {
         }
     }
 
-    function makeComment(comments, called = 0) {
-        const commentString = [];
-        for (let i = 0; i < comments.length; i++) {
-            commentString.push(`
-                <div style="padding-left: ${40 * called}px;" class="mt-4">
-                    <div class="text-gray-400">
-                        <i class="fa fa-sort-up mr-2"></i>
-                        <strong>${comments[i].user}</strong> ${comments[i].time_ago}
-                    </div>
-                    <p class="text-gray-700">${comments[i].content}</p>
+    updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
+}
+
+function makeComment(comments: NewsComment[]) {
+    const commentString: string[] = [];
+    for (let i = 0; i < comments.length; i++) {
+        const comment = comments[i];
+        commentString.push(`
+            <div style="padding-left: ${40 * comment.level}px;" class="mt-4">
+                <div class="text-gray-400">
+                    <i class="fa fa-sort-up mr-2"></i>
+                    <strong>${comment.user}</strong> ${comment.time_ago}
                 </div>
-            `);
+                <p class="text-gray-700">${comment.content}</p>
+            </div>
+        `);
 
-            // comments 를 다시 호출하여 대댓글을 출력한다.
-            if (comments[i].comments.length > 0) {
-                commentString.push(makeComment(comments[i].comments, called + 1));
-            }
+        // comments 를 다시 호출하여 대댓글을 출력한다.
+        if (comment.comments.length > 0) {
+            commentString.push(makeComment(comment.comments));
         }
-
-        return commentString.join('');
     }
 
-    template = template.replace('{{__comments__}}', makeComment(newsContent.comments));
-    container.innerHTML = template;
+    return commentString.join('');
 }
 
 function router() {
